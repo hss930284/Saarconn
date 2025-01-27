b80c4b988291240a5a1683d49cc27e64fcfa6098
hss930284 <hss930284@tatatechnologies.com>
2025-01-27 13:46:57 +0530

    update 411

--- Saarconn/sysdesk/basedonexcel/new_file12.py
+++
@@ -1,8 +0,0 @@
-
-
-# Usage
-root = ET.Element('ROOT')
-arxml_structure = ARXMLStructure()
-arxml_structure.create_default_pkg_struct(root)
-print(arxml_structure.get_variable('root'))  # prints the value of 'root'
-print(arxml_structure.get_variable('autosar_folder'))  # prints the value of 'autosar_folder'
